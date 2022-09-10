13b1dffc7252c28abba4b48e78f6eb2088bfe435
arresha <boyananggara111@gmail.com>
2022-09-10 21:00:11 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,11 @@
+document.getElementById('year').innerHTML = new Date().getFullYear();
+
+function chat() {
+  alert('Halo Min Saya Mau Tanya nih...');
+}
+
+function chat() {
+  const message = 'Halo saya mau beli';
+  const whatsapp = 'https://wa.me/62895323284030?text=Halo Saya Mau Beli';
+  open(whatsapp, '_blank');
+}
